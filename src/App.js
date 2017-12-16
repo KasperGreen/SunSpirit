7d9cd222e8553c7b5c375ca28ec0e9afe1fc57ed
Kasper Green <kaspergreen@gmail.com>
2017-12-16 04:09:53 +0300

    Добавлен ReactJS

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import s from './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className={s.App}>
+        <header className={s['App-header']}>
+          <img src={logo} className={s['App-logo']} alt="logo" />
+          <h1 className={s['App-title']}>Welcome to React</h1>
+        </header>
+        <p className={s['App-intro']}>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
