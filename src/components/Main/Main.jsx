97e94d3f98a9423f665393e87a88b0d58cc8de5e
Kasper Green <kaspergreen@gmail.com>
2017-12-18 12:14:18 +0300

    уменьшенная шапка

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,20 +2,38 @@ import React, { PureComponent } from 'react'
 import s from './css'
 import { Party, PartyList } from 'exportComponents'
 
-
 export default class Main extends PureComponent {
 
+  state = {
+    loaded: false
+  }
 
   render () {
+    const {
+      state: {
+        loaded
+      }
+    } = this
     return (
-      <main className={s.wrapper}>
-        <Party/>
-        <PartyList/>
+      <main className={loaded ? s.loaded_wrapper : s.wrapper}>
+        <Party />
+        <PartyList />
       </main>
     )
   }
 
+  timeout
 
+  componentWillMount () {
+    this.timeout = setTimeout(
+      () => {
+        this.setState({loaded: true})
+      },
+      842)
+  }
 
+  componentWillUnmount () {
+    clearTimeout(this.timeout)
+  }
 
 }
