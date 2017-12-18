b6650909531d22a7e70162d2d954410e3fbbdfe0
Kasper Green <kaspergreen@gmail.com>
2017-12-18 07:31:06 +0300

    path fix

--- src/components/Party/Party.jsx
+++ src/components/Party/Party.jsx
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import s from './css'
 import _ from 'lodash'
+import bg_path from 'static/images/earth.jpg'
 
 const artists = [
   {
@@ -24,25 +25,27 @@ export default class Party extends PureComponent {
   render () {
     return (
       <div className={s.wrapper}>
+        <div className={s.background} style={{backgroundImage: `url(${bg_path})`}} />
         <article className={s.article}>
-        <header>
-          <h1 className={s.title}>Атмосфера 5.1.0</h1>
-          <cite className={s.genre}>downtempo rave</cite>
-        </header>
-        <nav className={s.persons}>
-          <ul>
-            {_.map(artists, ({href, name, special}, key) => {
-              return (
-                <li key={key}>
-                  <a {...{href}}
-                    target={'_blank'}
-                     className={special ? s.special_person : s.person}
-                  >{name}</a>
-                </li>
-              )
-            })}
-          </ul>
-        </nav></article>
+          <header>
+            <h1 className={s.title}>Атмосфера 5.1.0</h1>
+            <cite className={s.genre}>downtempo rave</cite>
+          </header>
+          <nav className={s.persons}>
+            <ul>
+              {_.map(artists, ({href, name, special}, key) => {
+                return (
+                  <li key={key}>
+                    <a {...{href}}
+                       target={'_blank'}
+                       className={special ? s.special_person : s.person}
+                    >{name}</a>
+                  </li>
+                )
+              })}
+            </ul>
+          </nav>
+        </article>
       </div>
     )
   }
