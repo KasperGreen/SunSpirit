a12b3e035f40a216ffcf6e4be9f27e060fc0ea9f
Kasper Green <kaspergreen@gmail.com>
2017-12-18 03:45:33 +0300

    PartyList styling

--- src/components/PartyList/PartyList.jsx
+++ src/components/PartyList/PartyList.jsx
@@ -1,48 +1,53 @@
 import React, { PureComponent } from 'react'
 import s from './css'
+import background_url from 'static/images/PartyList_background.jpg'
+import _ from 'lodash'
+
+const partys = [
+  {
+    date: '06/10',
+    name: 'Intelligent Sound'
+  },
+  {
+    date: '27/10',
+    name: 'Ambient Sphere'
+  },
+  {
+    date: '09/11',
+    name: 'Analog Voices',
+  },
+  {
+    date: '30/11',
+    name: 'Broken Beats'
+  },
+  {
+    date: '21/12',
+    name: 'Downtempo Rave',
+    active: true
+  },
+
+]
+
 
 export default class PartyList extends PureComponent {
 
   render () {
     return (
       <nav className={s.wrapper}>
-        <ul>
-          <li>
-            <h2>
-              <time>06/10</time>
-
-              Intelligent Sound
-            </h2>
-          </li>
-          <li>
-            <h2>
-              <time>27/10</time>
-
-              Ambient Sphere
-            </h2>
-          </li>
-          <li>
-            <h2>
-              <time>09/11</time>
-
-              Analog Voices
-            </h2>
-          </li>
-          <li>
-            <h2>
-              <time>30/11</time>
-
-              Broken Beats
-            </h2>
-          </li>
-          <li>
-            <h2>
-              <time>21/12</time>
-
-              Downtempo Rave
-            </h2>
-          </li>
-
+        <div className={s.blured_background} style={{backgroundImage: `url(${background_url})`}}/>
+        <ul className={s.list}>
+          {_.map(partys, ({date, name, active}, key) => {
+            return(
+              <li className={active ? s.active_party : s.party}>
+                <time className={s.date}>
+                  {date}
+                </time>
+                <h2 className={s.name}>
+                  {name}
+                </h2>
+              </li>
+            )
+          })}
         </ul>
       </nav>
     )
